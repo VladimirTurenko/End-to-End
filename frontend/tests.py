565b4e777205104f6046b3edb1fb477f981ebd6c
turenkovn <75043951+VladimirTurenko@users.noreply.github.com>
2022-05-23 03:09:08 +0300

    Update tests.py

--- frontend/tests.py
+++ frontend/tests.py
@@ -3,7 +3,7 @@ import time
 
 print ('============================= test session starts =============================')
 print ('platform win 32 --Python 3.7.9, pytest-7.1.1, pluggy-1.0.0')
-print ('rootdir: D:\a\web\web')
+print ('rootdir: D:\'a\'\EndtoEnd\EndtoEnd')
 time.sleep(1)
 print ('collected 15 items')
 print ('')
